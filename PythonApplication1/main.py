db3031ef864351fb9c8bac3934486483ed5391a4
Jbridg12 <48564669+Jbridg12@users.noreply.github.com>
2021-08-30 12:26:31 -0400

    basically done just a lil more

--- PythonApplication1/main.py
+++ PythonApplication1/main.py
@@ -8,31 +8,61 @@ Functionality:
 import csv
 
 def analyze_applicant1(applicant):
+
     """
     Given the GPAs of a single applicant, return True if they are qualified 
-    Qualification: Anapplicant is qualified if. . .
-    −The average of all their grades i s above 85-
+    Qualification: Anapplicant is qualified if...
+    −The average of all their grades is above 85
+
     @param applicant: a list of GPAs(int ege rs)
     @return True if the applicant qualifies
     """
 
-    return True  #Simulate an assessment
-    
-    #c a l l the function 
-    if analyze_applicant1( applicant ):
-        print("They passed! :)")
+    av = (applicant[0] + applicant[1] + applicant[2] + applicant[3])/4
+    if(av > 85):
+        return True
     else:
-        print("They didn’t pass! :(")
+        return False
+    
 
 def analyze_applicant2(applicant):
-    return True
+    low = 100
+    for i in appplicant:
+        if(i < low):
+            low = i
+
+    if(low < 65):
+        return True
+    else:
+        return False
+
 def analyze_applicant3(applicant):
-    return True
-def analyze_applicant3(applicant):
+    count = 0
+    for i in applicant:
+        if(i > 85):
+            count += 1
+
+    if(count >= 4):
+        return True
+    else:
+        return False
+
+def analyze_applicant4(applicant):
     return True
 
-gpas = open('applicants.csv')
-ar = csv.reader(gpas)
 
-for row in ar:
-    print(row)
+with open('applicants.csv') as gpas:
+    with open('results.csv', 'w') as results:
+
+        ar = csv.reader(gpas)
+        rw = csv.writer(results)
+
+        rw.writerow(["Analysis #1, Analysis #2, Analysis #3, Analysis #4"])
+
+        skip = True
+        for row in ar:
+            if(skip):
+                print(row)
+                skip = False
+            else:
+                rw.writerow([analyze_applicant1(row), analyze_applicant2(row), analyze_applicant3(row), analyze_applicant4(row)])
