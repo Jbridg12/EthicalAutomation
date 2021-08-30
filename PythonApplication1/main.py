d9305ea0342d59e26e6e33db8684d949940a09e3
Jbridg12 <48564669+Jbridg12@users.noreply.github.com>
2021-08-30 11:40:03 -0400

    got source

---
+++ PythonApplication1/main.py
@@ -0,0 +1,22 @@
+
+import csv
+
+def analyze_applicant1 ( applicant ):
+    """
+    Given the GPAs of a single applicant, return True if they are qualified 
+    Qualification: Anapplicant is qualified if. . .
+    −The average of all their grades i s above 85-
+    @param applicant: a list of GPAs(int ege rs)
+    @return True if the applicant qualifies
+    """
+
+    return True  #Simulate an assessment
+    
+    #define a list of dummy scores 
+    applicant = [1, 2, 3, 4, 5, 6]
+    
+    #c a l l the function 
+    if analyze_applicant1( applicant ):
+        print("They passed! :)")
+    else:
+        print("They didn’t pass! :(")
